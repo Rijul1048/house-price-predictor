fedff9ae19172da7cbfebf7a3befef843af457d1
unknown <rijul.ahuja.vishal@gmail.com>
2026-01-07 23:15:51 +0530

    security increased

--- backend/predictor/views.py
+++ backend/predictor/views.py
@@ -1,3 +1,16 @@
+import joblib
 from django.shortcuts import render
 
-# Create your views here.
+model = joblib.load('../ml/house_price_model.pkl')
+
+def home(request):
+    prediction = None
+
+    if request.method == 'POST':
+        area = float(request.POST['area'])
+        bedrooms = int(request.POST['bedrooms'])
+        bathrooms = int(request.POST['bathrooms'])
+
+        prediction = model.predict([[area, bedrooms, bathrooms]])[0]
+
+    return render(request, 'home.html', {'prediction': prediction})
